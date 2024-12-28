b90e5325358921f6d5d8a4522074518d45dfead5
Szymon Migas <ataszymon@gmail.com>
2024-12-28 17:26:47 +0100

    added country generator

---
+++ data_generator/misc/countries.py
@@ -0,0 +1,16 @@
+import json
+import pyodbc
+
+
+with open("misc/cities.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+    countries = data
+
+def fill_countries(cursor: pyodbc.Cursor):
+    for i, country in enumerate(countries):
+        insert_country(cursor, i + 1, country["country_name"])
+
+def insert_country(cursor: pyodbc.Cursor, id: int, country_name: str):
+    sql_insert = f"insert into dbo.Countries (CountryID, CountryName) values ({str(id)}, '{country_name}')"
+    cursor.execute(sql_insert)
+
