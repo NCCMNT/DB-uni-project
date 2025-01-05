d81ff4a484105a2b65a753fcadf6df97815fc996
Szymon Migas <ataszymon@gmail.com>
2025-01-05 22:47:42 +0100

    fixed course attendance view

--- views/service-raports/authorized-students-for-course-meeting.sql
+++ views/service-raports/authorized-students-for-course-meeting.sql
@@ -1,14 +1,19 @@
-create view UsersAuthorizedForCourseMeeting as
-select MeetingID, O.UserID
+with users_course_payments as
+         (select O.UserID, OD.ServiceID, sum(Amount) paid, max(PayDate) maxPaymentDate
           from Payments
-                   inner join dbo.OrderDetails OD on Payments.OrderDetailID = OD.OrderDetailID
+                   inner join dbo.OrderDetails OD
+                              on Payments.OrderDetailID = OD.OrderDetailID and ServiceTypeID = 5
                    inner join dbo.Orders O on OD.OrderID = O.OrderID
-                   inner join dbo.ServiceTypes ST on OD.ServiceTypeID = ST.ServiceTypeID
-                   left join dbo.CoursesMeetings CM on CM.CourseID = OD.ServiceID
-          where ServiceTypeName = 'Course'
-            and PayDate < CM.StartDate
-          union
-          select CM2.MeetingID, UserID
-          from HeadTeacherPaymentPostponements
-                   inner join ServiceTypes ST2 on ST2.ServiceTypeName = 'Course'
-                   inner join dbo.CoursesMeetings CM2 on CM2.CourseID = ServiceID
+          group by O.UserID, OD.ServiceID)
+select MeetingID, UserID
+from users_course_payments U
+         inner join dbo.Courses C on U.ServiceID = C.CourseID
+         left join dbo.CoursesMeetings CM on C.CourseID = CM.CourseID
+where MeetingID is not null
+  and DATEDIFF(hour, maxPaymentDate, StartDate) > 72
+  and paid >= TotalPrice
+union
+select CM2.MeetingID, UserID
+from HeadTeacherPaymentPostponements H
+         inner join dbo.CoursesMeetings CM2 on CM2.CourseID = ServiceID
+where H.ServiceTypeID = 5
