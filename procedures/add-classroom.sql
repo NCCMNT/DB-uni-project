069be8366f69bafcac52aab72f03994332b2a795
Szymon Migas <ataszymon@gmail.com>
2025-01-10 23:36:14 +0100

    created procedures for misc section

---
+++ procedures/add-classroom.sql
@@ -0,0 +1,29 @@
+CREATE PROCEDURE AddClassroom
+    @AddressID int,
+    @RoomNumber int,
+    @Capacity int
+AS
+    BEGIN
+        SET NOCOUNT ON
+        DECLARE @ClassroomID int = (SELECT MAX(ClassroomID) + 1 FROM Classrooms)
+        IF @AddressID NOT IN (SELECT AddressID FROM Addresses)
+            BEGIN
+                RAISERROR ('Address does not exist', 16, 1)
+            END
+        IF @RoomNumber IS NULL OR @RoomNumber < 0
+            BEGIN
+                RAISERROR ('Room Number cannot be null or less than 0', 16, 1)
+            END
+        IF @Capacity IS NULL OR @Capacity < 0
+            BEGIN
+                RAISERROR ('Capacity cannot be null or less than 0', 16, 1)
+            END
+        IF @RoomNumber IN (SELECT RoomNo FROM Classrooms WHERE AddressID = @AddressID)
+            BEGIN
+                RAISERROR ('Classroom already exists at this address', 16, 1)
+            END
+
+        INSERT INTO Classrooms (ClassroomID, AddressID, RoomNo, Capacity)
+        VALUES (@ClassroomID, @AddressID, @RoomNumber, @Capacity)
+        PRINT 'Classroom added successfully'
+    END
