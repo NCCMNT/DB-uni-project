2464645bdd7e91c8494c3bc410b831298d869eea
NCCMNT <chmielewski2003@gmail.com>
2025-01-25 13:25:04 +0100

    study indexes

---
+++ indexes/remove-indexes.sql
@@ -0,0 +1,9 @@
+-- DROP STUDIES INDEXES
+DROP INDEX StudiesIDX ON Studies;
+DROP INDEX StudentsIDX ON Students;
+DROP INDEX InternshipsIDX ON Internships;
+DROP INDEX StudySemestersIDX ON StudySemesters;
+DROP INDEX StationaryStudyIDX ON StationaryStudy;
+DROP INDEX StudiesMeetingsIDX ON StudiesMeetings;
+DROP INDEX StudyMeetupsIDX ON StudyMeetups;
+DROP INDEX FinalExamsIDX ON FinalExams;
